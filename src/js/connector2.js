e4a7112d676eab23a0b6e67920e7c9acd97cc78e
Jordan <jjohnson@myself.com>
2023-02-08 21:59:01 -0600

    connector 2 update

--- src/js/connector2.js
+++ src/js/connector2.js
@@ -1,5 +1,8 @@
+console.log('Hello World from Connector2.js')
+
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
         // return an array of card badges for the given card
+        return [];
     }
 });
