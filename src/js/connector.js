72c37285bc117eda8a93cb4a111558b2e81d874f
Jordan <jjohnson@myself.com>
2023-02-11 11:18:24 -0600

    Testing update

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,3 +1,5 @@
+console.log('Hello World from Connector.js')
+
 window.TrelloPowerUp.initialize({
     "card-badges": function (t, opts) {
       let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
