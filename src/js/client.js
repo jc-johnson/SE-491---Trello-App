032f257c0d54ab062e2f85caa58b6866cd0df5b9
Jordan <jjohnson@myself.com>
2023-02-23 21:07:01 -0600

    Icon updates for board button

--- src/js/client.js
+++ src/js/client.js
@@ -1,3 +1,7 @@
+var GLITCH_ICON = 'https://cdn.glitch.com/2442c68d-7b6d-4b69-9d13-feab530aa88e%2Fglitch-icon.svg?1489773457908';
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+var WHITE_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
+
 var boardButtonCallback = function(t){
   return t.popup({
     title: 'Popup List Example',
