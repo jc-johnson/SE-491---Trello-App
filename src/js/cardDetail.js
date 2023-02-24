fc3ea6e1539c2b4aa3bd7fe302682cb9c6941161
Jordan <jjohnson@myself.com>
2023-02-23 20:58:17 -0600

    Adding board button funcationality

--- src/js/cardDetail.js
+++ src/js/cardDetail.js
@@ -1,54 +1,194 @@
-window.TrelloPowerUp.initialize({
-    "card-detail-badges": function (t, opts) {
-      return t
-        .card("name")
-        .get("name")
-        .then(function (cardName) {
-          console.log("We just loaded the card name for fun: " + cardName);
+// We need to call initialize to get all of our capability handles set up and registered with Trello
+TrelloPowerUp.initialize({
+  // NOTE about asynchronous responses
+  // If you need to make an asynchronous request or action before you can reply to Trello
+  // you can return a Promise (bluebird promises are included at TrelloPowerUp.Promise)
+  // The Promise should resolve to the object type that is expected to be returned
+  'attachment-sections': function(t, options){
+    // options.entries is a list of the attachments for this card
+    // you can look through them and 'claim' any that you want to
+    // include in your section.
+
+    // we will just claim urls for Yellowstone
+    var claimed = options.entries.filter(function(attachment){
+      return attachment.url.indexOf('www.nps.gov/yell/') > -1;
+    });
+
+    // you can have more than one attachment section on a card
+    // you can group items together into one section, have a section
+    // per attachment, or anything in between.
+    if(claimed && claimed.length > 0){
+      // if the title for your section requires a network call or other
+      // potentially length operation you can provide a function for the title
+      // that returns the section title. If you do so, provide a unique id for
+      // your section
+      return [{
+        id: 'Yellowstone', // optional if you aren't using a function for the title
+        claimed: claimed,
+        icon: GLITCH_ICON,
+        title: 'Example Attachment Section: Yellowstone',
+        content: {
+          type: 'iframe',
+          // You'll need to sign the URL to ensure that the iframe can continue to
+          // communicate with Trello.
+          url: t.signUrl('./section.html', { arg: 'you can pass your section args here' }),
+          height: 230
+        }
+      }];
+    } else {
+      return [];
+    }
+  },
+  'attachment-thumbnail': function(t, options){
+    // options.url has the url of the attachment for us
+    // return an object (or a Promise that resolves to it) with some or all of these properties:
+    // url, title, image, modified (Date), created (Date), createdBy, modifiedBy
+    
+    // You should use this if you have useful information about an attached URL but it
+    // doesn't warrant pulling it out into a section via the attachment-sections capability
+    // for example if you just want to show a preview image and give it a better name
+    // then attachment-thumbnail is the best option
+    return {
+      url: options.url,
+      title: '👉 ' + options.url + ' 👈',
+      image: {
+        url: GLITCH_ICON,
+        logo: true // false if you are using a thumbnail of the content
+      },
+    };
+    
+    // if we don't actually have any valuable information about the url
+    // we can let Trello know like so:
+    // throw t.NotHandled();
+  },
+  'board-buttons': function(t, options){
+    return [{
+      // we can either provide a button that has a callback function
+      // that callback function should probably open a popup, overlay, or boardBar
+      icon: WHITE_ICON,
+      text: 'Popup',
+      callback: boardButtonCallback
+    }, {
+      // or we can also have a button that is just a simple url
+      // clicking it will open a new tab at the provided url
+      icon: WHITE_ICON,
+      text: 'URL',
+      url: 'https://trello.com/inspiration',
+      target: 'Inspiring Boards' // optional target for above url
+    }];
+  },
+  'card-badges': function(t, options){
+    return getBadges(t);
+  },
+  'card-buttons': function(t, options) {
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Open Popup',
+      callback: cardButtonCallback
+    }, {
+      // but of course, you could also just kick off to a url if that's your thing
+      icon: GRAY_ICON,
+      text: 'Just a URL',
+      url: 'https://developers.trello.com',
+      target: 'Trello Developer Site' // optional target for above url
+    }];
+  },
+  'card-detail-badges': function(t, options) {
+    return getBadges(t);
+  },
+  'card-from-url': function(t, options) {
+    // options.url has the url in question
+    // if we know cool things about that url we can give Trello a name and desc
+    // to use when creating a card. Trello will also automatically add that url
+    // as an attachment to the created card
+    // As always you can return a Promise that resolves to the card details
+    
+    return new Promise(function(resolve) {
+      resolve({
+        name: '💻 ' + options.url + ' 🤔',
+        desc: 'This Power-Up knows cool things about the attached url'
+      });
+    });
+    
+    // if we don't actually have any valuable information about the url
+    // we can let Trello know like so:
+    // throw t.NotHandled();
+  },
+  'format-url': function(t, options) {
+    // options.url has the url that we are being asked to format
+    // in our response we can include an icon as well as the replacement text
+    
+    return {
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: '👉 ' + options.url + ' 👈' 
+    };
+    
+    // if we don't actually have any valuable information about the url
+    // we can let Trello know like so:
+    // throw t.NotHandled();
+  },
+  'show-settings': function(t, options){
+    // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
+    // what should Trello show. We highly recommend the popup in this case as
+    // it is the least disruptive, and fits in well with the rest of Trello's UX
+    return t.popup({
+      title: 'Settings',
+      url: './settings.html',
+      height: 184 // we can always resize later, but if we know the size in advance, its good to tell Trello
+    });
+  },
   
-          return [
-            {
-              // dynamic badges can have their function rerun after a set number
-              // of seconds defined by refresh. Minimum of 10 seconds.
-              dynamic: function () {
-                // we could also return a Promise that resolves to this
-                // as well if we needed to do something async first
-                return {
-                  title: "Detail Badge",
-                  text: "Dynamic " + (Math.random() * 100).toFixed(0).toString(),
-                  color: randomBadgeColor(),
-                  refresh: 10, // in seconds
-                };
-              },
-            },
-            {
-              // its best to use static badges unless you need your badges
-              // to refresh you can mix and match between static and dynamic
-              title: "Detail Badge",
-              text: "Static",
-              color: null,
-            },
-            {
-              // card detail badges (those that appear on the back of cards)
-              // also support callback functions so that you can open for example
-              // open a popup on click
-              title: "Popup Detail Badge",
-              text: "Popup",
-              callback: function (t, opts) {
-                // function to run on click
-                // do something
-              },
-            },
-            {
-              // or for simpler use cases you can also provide a url
-              // when the user clicks on the card detail badge they will
-              // go to a new tab at that url
-              title: "URL Detail Badge",
-              text: "URL",
-              url: "https://trello.com/home",
-              target: "Trello Landing Page", // optional target for above url
-            },
-          ];
-        });
-    },
-  });
+  /*        
+      
+      🔑 Authorization Capabiltiies 🗝
+      
+      The following two capabilities should be used together to determine:
+      1. whether a user is appropriately authorized
+      2. what to do when a user isn't completely authorized
+      
+  */
+  'authorization-status': function(t, options){
+    // Return a promise that resolves to an object with a boolean property 'authorized' of true or false
+    // The boolean value determines whether your Power-Up considers the user to be authorized or not.
+    
+    // When the value is false, Trello will show the user an "Authorize Account" options when
+    // they click on the Power-Up's gear icon in the settings. The 'show-authorization' capability
+    // below determines what should happen when the user clicks "Authorize Account"
+    
+    // For instance, if your Power-Up requires a token to be set for the member you could do the following:
+    return t.get('member', 'private', 'token')
+    // Or if you needed to set/get a non-Trello secret token, like an oauth token, you could
+    // use t.storeSecret('key', 'value') and t.loadSecret('key')
+    .then(function(token){
+      if(token){
+        return { authorized: true };
+      }
+      return { authorized: false };
+    });
+    // You can also return the object synchronously if you know the answer synchronously.
+  },
+  'show-authorization': function(t, options){
+    // Returns what to do when a user clicks the 'Authorize Account' link from the Power-Up gear icon
+    // which shows when 'authorization-status' returns { authorized: false }.
+    
+    // If we want to ask the user to authorize our Power-Up to make full use of the Trello API
+    // you'll need to add your API from trello.com/app-key below:
+    let trelloAPIKey = '';
+    // This key will be used to generate a token that you can pass along with the API key to Trello's
+    // RESTful API. Using the key/token pair, you can make requests on behalf of the authorized user.
+    
+    // In this case we'll open a popup to kick off the authorization flow.
+    if (trelloAPIKey) {
+      return t.popup({
+        title: 'My Auth Popup',
+        args: { apiKey: trelloAPIKey }, // Pass in API key to the iframe
+        url: './authorize.html', // Check out public/authorize.html to see how to ask a user to auth
+        height: 140,
+      });
+    } else {
+      console.log("🙈 Looks like you need to add your API key to the project!");
+    }
+  }
+});
--- src/js/client.js
+++ src/js/client.js
@@ -1,14 +1,85 @@
-TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
-    return [{
-      icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
-      text: 'Estimate Size',
-      callback: function(t){
-        return t.popup({
-          title: "Estimation",
-          url: 'html/estimate.html'
-        });
+var boardButtonCallback = function(t){
+  return t.popup({
+    title: 'Popup List Example',
+    items: [
+      {
+        text: 'Open Modal',
+        callback: function(t){
+          return t.modal({            
+            url: '../html/modal.html', // The URL to load for the iframe
+            args: { text: 'Hello' }, // Optional args to access later with t.arg('text') on './modal.html'
+            accentColor: '#F2D600', // Optional color for the modal header 
+            height: 500, // Initial height for iframe; not used if fullscreen is true
+            fullscreen: true, // Whether the modal should stretch to take up the whole screen
+            callback: () => console.log('Goodbye.'), // optional function called if user closes modal (via `X` or escape)
+            title: 'Hello, Modal!', // Optional title for modal header
+            // You can add up to 3 action buttons on the modal header - max 1 on the right side.
+            actions: [{
+              icon: GRAY_ICON,
+              url: 'https://google.com', // Opens the URL passed to it.
+              alt: 'Leftmost',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: (tr) => tr.popup({ // Callback to be called when user clicks the action button.
+                title: 'Settings',
+                url: '../html/settings.html',
+                height: 164,
+              }),
+              alt: 'Second from left',
+              position: 'left',
+            }, {
+              icon: GRAY_ICON,
+              callback: () => console.log('🏎'),
+              alt: 'Right side',
+              position: 'right',
+            }],
+          })
+        }
+      },
+      {
+        text: 'Open Board Bar',
+        callback: function(t){
+          return t.boardBar({
+            url: './board-bar.html',
+            height: 200
+          })
+          .then(function(){
+            return t.closePopup();
+          });
+        }
       }
+    ]
+  });
+};
+
+TrelloPowerUp.initialize({
+  'card-buttons': function (t, opts) {
+    return [{
+      icon: 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg',
+      text: 'Open Popup',
+      callback: function(t) {
+          t.alert({
+            message: 'Hello World!'
+          })
+      },
+      condition: 'always'
     }];
-  }
+  },
+  'board-buttons': function(t, options){
+    return [{
+      // we can either provide a button that has a callback function
+      // that callback function should probably open a popup, overlay, or boardBar
+      icon: WHITE_ICON,
+      text: 'Popup',
+      callback: boardButtonCallback
+    }, {
+      // or we can also have a button that is just a simple url
+      // clicking it will open a new tab at the provided url
+      icon: WHITE_ICON,
+      text: 'URL',
+      url: 'https://trello.com/inspiration',
+      target: 'Inspiring Boards' // optional target for above url
+    }];
+  },
 });
