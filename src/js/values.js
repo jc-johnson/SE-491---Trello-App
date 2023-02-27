fa4e94a59c7fea53a31ec82d095d2fb128cfebed
fengwang324 <111786529+fengwang324@users.noreply.github.com>
2023-02-27 17:17:29 -0600

    update

--- src/js/values.js
+++ src/js/values.js
@@ -1,5 +1,9 @@
+
 const CLIENT_ID = '695030822144-rdkeqou0cenai8bguj6q159g0qrf3ri8.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyAqzEmB1ydpBLYNybMsjKL3xavYhj3fHPA';
+const CalendarScope = 'https://www.googleapis.com/auth/calendar';
 
 // Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+let tokenClient;
