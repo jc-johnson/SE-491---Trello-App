3ff82421314a0e14b56698674b63595bef5d0cd7
fengwang324 <111786529+fengwang324@users.noreply.github.com>
2023-02-27 20:23:58 -0600

    change location

--- src/js/client.js
+++ src/js/client.js
@@ -65,6 +65,32 @@ var onBtnClick = function (t, opts) {
   })
 };
 
+var onOauthClick = function handleAuthClick() {
+  tokenClient.callback = async (resp) => {
+    if (resp.error !== undefined) {
+      console.log('login error');
+      window.TrelloPowerUp.iframe().alert({
+          message: 'google login error'
+      });
+      throw (resp);
+    }
+    console.log('login success');
+    window.TrelloPowerUp.iframe().alert({
+      message: 'google login success'
+    }); 
+    isOauth = true;
+  };
+
+  if (gapi.client.getToken() === null) {
+    // Prompt the user to select a Google Account and ask for consent to share their data
+    // when establishing a new session.
+    tokenClient.requestAccessToken({prompt: 'consent'});
+  } else {
+    // Skip display of account chooser and consent dialog for an existing session.
+    tokenClient.requestAccessToken({prompt: ''});
+  }
+}
+
 TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
     return [{
@@ -91,7 +117,7 @@ TrelloPowerUp.initialize({
       icon: GRAY_ICON,
       text: 'Google Login',
       condition: 'always',
-      callback: handleAuthClick()
+      callback: onOauthClick
     }];
   },
   // 'board-buttons': function(t, options){
--- src/js/oauth2login.js
+++ src/js/oauth2login.js
@@ -1,28 +1,4 @@
-function handleAuthClick() {
-    tokenClient.callback = async (resp) => {
-      if (resp.error !== undefined) {
-        console.log('login error');
-        window.TrelloPowerUp.iframe().alert({
-            message: 'google login error'
-        });
-        throw (resp);
-      }
-      console.log('login success');
-      window.TrelloPowerUp.iframe().alert({
-        message: 'google login success'
-      }); 
-      isOauth = true;
-    };
 
-    if (gapi.client.getToken() === null) {
-      // Prompt the user to select a Google Account and ask for consent to share their data
-      // when establishing a new session.
-      tokenClient.requestAccessToken({prompt: 'consent'});
-    } else {
-      // Skip display of account chooser and consent dialog for an existing session.
-      tokenClient.requestAccessToken({prompt: ''});
-    }
-}
 
 function handleSignoutClick() {
     const token = gapi.client.getToken();
